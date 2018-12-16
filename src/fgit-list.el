9a2136b23ccd53856c7f68a31b0e75bdc0646693
Florent Georges <fgeorges@fgeorges.org>
2018-12-16 22:31:50 +0100

    Change the naming convention.

--- src/fgit-list.el
+++ src/fgit-list.el
@@ -26,7 +26,7 @@
 ;; repositories:
 ;;
 ;;     (load-library "fgit-list")
-;;     (setq fgit:repositories
+;;     (setq fgit/repositories
 ;;           '(((:path . "/home/me/projects/elisp/foobar/")  (:fetch . t))
 ;;             ((:path . "/home/me/projects/elisp/somelib/"))
 ;;             ((:path . "/home/me/projects/git/fourtytwo/") (:fetch . t))))
@@ -38,7 +38,7 @@
 ;;; Code:
 
 ;; TODO: Use defcustom instead.
-(defvar fgit:repositories ()
+(defvar fgit/repositories ()
   "The list of all Git repositories on this system.
 
 This is a list of alists, one per repo.  Each alist must have an entry `:path',
@@ -57,51 +57,51 @@ Unix-like systems):
 > locate applypatch-msg | grep --color .git/hooks/applypatch-msg
 > find /home/me/ -name 'applypatch-ms*' | grep --color .git/hooks/applypatch-msg")
 
-(defvar fgit:fetch-by-default nil
+(defvar fgit/fetch-by-default nil
   "Whether to fetch for all repositories.
 
 When set to `t', it is aquivalent to set `(:fetch . t)' on all repos in
-`fgit:repositories'.")
+`fgit/repositories'.")
 
-(defconst fgit:buffer-name "*fgit-repositories*")
+(defconst fgit/buffer-name "*fgit-repositories*")
 
-(defface fgit:clean
+(defface fgit/clean
   '((((class color) (background light)) :foreground "#5ac85a" :weight bold)
     (((class color) (background  dark)) :foreground "#4d994d" :weight bold))
   "Face for clean repos.")
 
-(defface fgit:dirty
+(defface fgit/dirty
  '((((class color) (background light)) :foreground "#ff4545" :weight bold)
    (((class color) (background  dark)) :foreground "#b26767" :weight bold))
   "Face for dirty repos.")
 
-(defface fgit:section
+(defface fgit/section
   '((((class color) (background light)) :foreground "#a28339")
     (((class color) (background  dark)) :foreground "#beb068"))
   "Face for sub sections.")
 
-(defface fgit:repo-name
+(defface fgit/repo-name
   '((t :inherit (bold)))
   "Face for repo paths.")
 
-(defface fgit:repo-path
+(defface fgit/repo-path
   '()
   "Face for repo paths.")
 
-(defun fgit:insert (str face)
+(defun fgit/insert (str face)
   (when str
     (insert (propertize str 'face face))))
 
-(defun fgit:insert-repo (repo)
+(defun fgit/insert-repo (repo)
   (if (not (string-match "^\\(.*[/\\]\\)\\([^/\\]+\\)\\([/\\]\\)?$" repo))
-      (fgit:insert repo 'fgit:repo-name)
-    (fgit:insert (match-string 1 repo) 'fgit:repo-path)
-    (fgit:insert (match-string 2 repo) 'fgit:repo-name)
-    (fgit:insert (match-string 3 repo) 'fgit:repo-path)))
+      (fgit/insert repo 'fgit/repo-name)
+    (fgit/insert (match-string 1 repo) 'fgit/repo-path)
+    (fgit/insert (match-string 2 repo) 'fgit/repo-name)
+    (fgit/insert (match-string 3 repo) 'fgit/repo-path)))
 
-(defun fgit:start-magit-at (point)
+(defun fgit/start-magit-at (point)
   (interactive "d")
-  (let ((repo (get-char-property point :fgit:repo)))
+  (let ((repo (get-char-property point :fgit/repo)))
     (unless repo
       (error "No repo at: %d" point))
     (unless (fboundp 'magit-status-internal)
@@ -111,36 +111,36 @@ When set to `t', it is aquivalent to set `(:fetch . t)' on all repos in
         (error "Magit not found")))
     (magit-status-internal repo)))
 
-(defun fgit:overlay-at (point)
+(defun fgit/overlay-at (point)
   (let ((overlays (overlays-at point))
         found)
     (dolist (ol overlays)
-      (when (overlay-get ol :fgit:repo)
+      (when (overlay-get ol :fgit/repo)
         (when found
-          (error "More than one overlay with prop :fgit:repo at %d" point))
+          (error "More than one overlay with prop :fgit/repo at %d" point))
         (setq found ol)))
     found))
 
-(defun fgit:toggle-folding (point)
+(defun fgit/toggle-folding (point)
   (interactive "d")
-  (let ((ol (fgit:overlay-at point)))
+  (let ((ol (fgit/overlay-at point)))
     (when (not ol)
-      (error "No overlay with prop :fgit:repo at %d" point))
-    (let ((folded (overlay-get ol :fgit:folded)))
+      (error "No overlay with prop :fgit/repo at %d" point))
+    (let ((folded (overlay-get ol :fgit/folded)))
       (goto-char (overlay-start ol))
       (if folded
           (outline-show-subtree)
         (outline-hide-subtree))
-      (overlay-put ol :fgit:folded (not folded)))))
+      (overlay-put ol :fgit/folded (not folded)))))
 
-(defun fgit:pop-buffer ()
-  (let ((buffer (get-buffer fgit:buffer-name)))
+(defun fgit/pop-buffer ()
+  (let ((buffer (get-buffer fgit/buffer-name)))
     (if buffer
         (progn
           (pop-to-buffer buffer)
           (read-only-mode 0)
           (erase-buffer))
-      (setq buffer (get-buffer-create fgit:buffer-name))
+      (setq buffer (get-buffer-create fgit/buffer-name))
       (pop-to-buffer buffer)
       ;; it seems that `outline-minor-mode' tries to access the local map before
       ;; it is set (`local-set-key' would have avoided that, had they used it)
@@ -149,15 +149,15 @@ When set to `t', it is aquivalent to set `(:fetch . t)' on all repos in
       (outline-minor-mode 1)
       (toggle-truncate-lines 1)
       (local-set-key (kbd "q")     'bury-buffer)
-      (local-set-key (kbd "RET")   'fgit:start-magit-at)
-      (local-set-key (kbd "<tab>") 'fgit:toggle-folding))
+      (local-set-key (kbd "RET")   'fgit/start-magit-at)
+      (local-set-key (kbd "<tab>") 'fgit/toggle-folding))
     buffer))
 
-(defun fgit:git-to-string (repo options)
+(defun fgit/git-to-string (repo options)
   (let ((default-directory repo))
     (shell-command-to-string (concat "git " options))))
 
-(defun fgit:repo-file-status (line)
+(defun fgit/repo-file-status (line)
   (let ((index (elt line 0))
         (fs    (elt line 1))
         (file  (substring line 3)))
@@ -175,45 +175,45 @@ When set to `t', it is aquivalent to set `(:fetch . t)' on all repos in
           (t
            (error "Logic error, cannot happen!")))))
 
-(defun fgit:repo-commit-status (line)
+(defun fgit/repo-commit-status (line)
   (let ((arrow  (elt line 0))
         (commit (substring line 1)))
     (when (and (/= arrow ?<) (/= arrow ?>))
       (error "Arrow is neither < or >: %s" line))
     (cons arrow commit)))
 
-(defun fgit:current-upstream (repo)
-  (let ((output (fgit:git-to-string repo "rev-parse --abbrev-ref @{upstream}")))
+(defun fgit/current-upstream (repo)
+  (let ((output (fgit/git-to-string repo "rev-parse --abbrev-ref @{upstream}")))
     (if (not (string-prefix-p "fatal: " output))
         (substring output 0 -1) ; remove end of line
-      (let* ((output  (fgit:git-to-string repo "rev-parse --abbrev-ref HEAD"))
+      (let* ((output  (fgit/git-to-string repo "rev-parse --abbrev-ref HEAD"))
              ;; TODO: "origin" should be in a "default variable", overridable in
              ;; the repo alist itself...
              (branch  (concat "origin/" (substring output 0 -1))) ; remove end of line
              (options (concat "rev-list --left-right " branch "..." branch))
-             (output  (fgit:git-to-string repo options)))
+             (output  (fgit/git-to-string repo options)))
         (unless (string-prefix-p "fatal: " output)
           branch)))))
 
-(defun fgit:repo-status (repo)
+(defun fgit/repo-status (repo)
   (let (ahead behind modified deleted unknown)
-    (let ((upstream (fgit:current-upstream repo)))
+    (let ((upstream (fgit/current-upstream repo)))
       (when upstream
         (let* ((options (concat "rev-list --left-right ..." upstream))
-               (commits (fgit:git-to-string repo options)))
+               (commits (fgit/git-to-string repo options)))
           (dolist (line (split-string commits "\n"))
             (when (> (length line) 0)
-              (let* ((res    (fgit:repo-commit-status line))
+              (let* ((res    (fgit/repo-commit-status line))
                      (arrow  (car res))
                      (commit (cdr res)))
                 (cond ((= arrow ?<) (push commit ahead))
                       ((= arrow ?>) (push commit behind))
                       (t
                        (error "Unknown porcelain commit arrow: '%c'" arrow)))))))))
-    (let ((output (fgit:git-to-string repo "status --porcelain")))
+    (let ((output (fgit/git-to-string repo "status --porcelain")))
       (dolist (line (split-string output "\n"))
         (when (> (length line) 0)
-          (let* ((file   (fgit:repo-file-status line))
+          (let* ((file   (fgit/repo-file-status line))
                  (status (car file))
                  (path   (cdr file)))
             (cond ((= status ?M) (push path modified))
@@ -228,30 +228,30 @@ When set to `t', it is aquivalent to set `(:fetch . t)' on all repos in
      (cons :deleted  deleted)
      (cons :unknown  unknown))))
 
-(defun fgit:check-repos (arg)
+(defun fgit/check-repos (arg)
   "Check the status of all the Git repositories on the system.
 
-The repositories are those in `fgit:repositories'.  Invoked with a universal
+The repositories are those in `fgit/repositories'.  Invoked with a universal
 argument (aka `C-u'), it performs a `git fetch' before checking each repo.
 Display the result in a temporary buffer."
   (interactive "P")
   (let ((dofetch (and arg (yes-or-no-p "Do fetch? "))))
-    (fgit:pop-buffer)
+    (fgit/pop-buffer)
     (insert "List all Git repositories on this system, and whether they are clean or not.\n")
     (insert "- TAB: show/hide details of a non-clean (or \"dirty\") repo\n")
     (insert "- RET: on a repository, opens Magit (the status screen)\n")
     (insert "- q: bury the buffer\n")
     (insert "\n")
-    (dolist (repo fgit:repositories)
+    (dolist (repo fgit/repositories)
       (when (stringp repo)
         (setq repo (list (cons :path repo))))
       (let ((path  (cdr (assq :path  repo)))
             (fetch (cdr (assq :fetch repo))))
 	(unless path
-	  (error "No path in the fgit:repositories entry: %O" repo))
-        (when (and dofetch (or fetch fgit:fetch-by-default))
-          (fgit:git-to-string path "fetch"))
-        (let* ((status   (fgit:repo-status path))
+	  (error "No path in the fgit/repositories entry: %O" repo))
+        (when (and dofetch (or fetch fgit/fetch-by-default))
+          (fgit/git-to-string path "fetch"))
+        (let* ((status   (fgit/repo-status path))
                (ahead    (cdr (assq :ahead    status)))
                (behind   (cdr (assq :behind   status)))
                (modified (cdr (assq :modified status)))
@@ -264,41 +264,41 @@ Display the result in a temporary buffer."
                (hasunk   (> (length unknown)  0))
                (start    (point)))
           (cond ((not (or hasmod hasdel hasunk isahead isbehind))
-                 (fgit:insert "* " 'fgit:clean)
-                 (fgit:insert-repo path)
+                 (fgit/insert "* " 'fgit/clean)
+                 (fgit/insert-repo path)
                  (insert (make-string (max 2 (- 70 (length path))) ? ))
-                 (fgit:insert "clean" 'fgit:clean)
+                 (fgit/insert "clean" 'fgit/clean)
                  (insert "\n"))
                 (t
-                 (fgit:insert "* " 'fgit:dirty)
-                 (fgit:insert-repo path)
+                 (fgit/insert "* " 'fgit/dirty)
+                 (fgit/insert-repo path)
                  (insert (make-string (max 2 (- 70 (length path))) ? ))
-                 (fgit:insert "dirty" 'fgit:dirty)
+                 (fgit/insert "dirty" 'fgit/dirty)
                  (insert "\n")
                  (when isahead
-                   (fgit:insert "** ahead" 'fgit:section)
+                   (fgit/insert "** ahead" 'fgit/section)
                    (insert "\n" (int-to-string (length ahead)) " commit(s) ahead\n"))
                  (when isbehind
-                   (fgit:insert "** behind" 'fgit:section)
+                   (fgit/insert "** behind" 'fgit/section)
                    (insert "\n" (int-to-string (length behind)) " commit(s) behind\n"))
                  (when hasmod
-                   (fgit:insert "** modified" 'fgit:section)
+                   (fgit/insert "** modified" 'fgit/section)
                    (insert "\n")
                    (dolist (file modified)
                      (insert file "\n")))
                  (when hasdel
-                   (fgit:insert "** deleted" 'fgit:section)
+                   (fgit/insert "** deleted" 'fgit/section)
                    (insert "\n")
                    (dolist (file deleted)
                      (insert file "\n")))
                  (when hasunk
-                   (fgit:insert "** unknown" 'fgit:section)
+                   (fgit/insert "** unknown" 'fgit/section)
                    (insert "\n")
                    (dolist (file unknown)
                      (insert file "\n")))))
           (let ((ol (make-overlay start (point))))
-            (overlay-put ol :fgit:repo   path)
-            (overlay-put ol :fgit:folded t))))
+            (overlay-put ol :fgit/repo   path)
+            (overlay-put ol :fgit/folded t))))
       (redisplay))
     ;; collapse all repos
     (outline-hide-sublevels 1)
